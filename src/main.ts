38cfdab9a4d724977edf6b97a2f9ae5b1d945976
mokuteki <kondratiuk011@gmail.com>
2022-08-08 22:46:55 +0300

    feat: fastify adapter, config module, .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
