484cb62a72bc98b36791b84b1b5b8f5466e6961f
mokuteki <kondratiuk011@gmail.com>
2023-04-03 23:57:35 +0300

    fix(exception-handling): rename exception filter

--- src/common/error-to-graph.exception-filter.ts
+++ src/common/error-to-graph.exception-filter.ts
@@ -1,16 +1,21 @@
-import { ArgumentsHost, BadRequestException, Catch } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql';
 import { DomainError } from './domain-error';
 import { HttpExceptionErrorMap } from './http-exception-error-map';
 
 @Catch()
-export class AllExceptionFilter
+export class ErrorToGraphQLExceptionFilter
   implements GqlExceptionFilter<DomainError, GraphQLError>
 {
   public catch(error: DomainError, host: ArgumentsHost): GraphQLError {
     const exception =
-      HttpExceptionErrorMap.get(error.message) || new BadRequestException();
+      HttpExceptionErrorMap.get(error.message) ||
+      new InternalServerErrorException();
 
     const gqlError = new GraphQLError(error.message, {
       extensions: {
